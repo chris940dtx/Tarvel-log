f88777a37776e433616825d2e7a6d9fccde6db46
chris940dtx <christophertex1@gmail.com>
2025-08-27 20:47:16 -0500

    fixed double routing from App.js and index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './login';
-import Register from './register';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
